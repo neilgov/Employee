4d35ed0ffce8efd7eb71f972b71f8d2e8c28be58
Neil Govender <neilgovender2010@gmail.com>
2022-08-16 12:28:18 +0200

    Initial commit

---
+++ Employees.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Employees.Entity;
+namespace Employees.Persistence
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<associate> Employees { get; set; }
+      
+    }
+}
---
+++ Employees.Persistence/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Employees.Persistence
+{
+    public class Class1
+    {
+    }
+}
